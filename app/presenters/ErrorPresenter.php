d51781cb6c558c1367a92bee4eb5a653e94ed4e2
attreid <attreid@gmail.com>
2016-09-06 12:57:00 +0200

    update

--- app/presenters/ErrorPresenter.php
+++ app/presenters/ErrorPresenter.php
@@ -3,6 +3,8 @@
 namespace App\Presenters;
 
 use Nette;
+use Nette\Application\IResponse;
+use Nette\Application\Request;
 use Nette\Application\Responses;
 use Tracy\ILogger;
 
@@ -20,14 +22,15 @@ class ErrorPresenter implements Nette\Application\IPresenter
 	}
 
 	/**
-	 * @return Nette\Application\IResponse
+	 * @param Request $request
+	 * @return IResponse
 	 */
-	public function run(Nette\Application\Request $request)
+	public function run(Request $request)
 	{
 		$e = $request->getParameter('exception');
 
 		if ($e instanceof Nette\Application\BadRequestException) {
-			// $this->logger->log("HTTP code {$e->getCode()}: {$e->getMessage()} in {$e->getFile()}:{$e->getLine()}", 'access');
+			$this->logger->log("HTTP code {$e->getCode()}: {$e->getMessage()} in {$e->getFile()}:{$e->getLine()}", 'access');
 			list($module, , $sep) = Nette\Application\Helpers::splitName($request->getPresenterName());
 			return new Responses\ForwardResponse($request->setPresenterName($module . $sep . 'Error4xx'));
 		}
