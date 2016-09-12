87dee7ed58cc20beffb2e3406ba9feb5ddfb669b
attreid <attreid@gmail.com>
2016-09-12 10:50:25 +0200

    update

--- www/index.php
+++ www/index.php
@@ -1,12 +1,12 @@
 <?php
 
 if (file_exists(__DIR__ . '/../temp/maintenance')) {
-    if (isset($_GET['maintenanceOff']) || (isset($argv) && $argv[1] == 'maintenanceOff')) {
-        unlink(__DIR__ . '/../temp/maintenance');
-        echo "Maintenance off\n";
-        exit;
-    }
-    require '.maintenance.php';
+	if (isset($_GET['maintenanceOff']) || (isset($argv) && $argv[1] == 'maintenanceOff')) {
+		unlink(__DIR__ . '/../temp/maintenance');
+		echo "Maintenance off\n";
+		exit;
+	}
+	require '.maintenance.php';
 }
 
 $container = require __DIR__ . '/../app/bootstrap.php';
