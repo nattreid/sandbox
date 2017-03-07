b0120d6cc96bd57768231bf45c229fcc2a1e89bc
attreid <attreid@gmail.com>
2017-03-07 09:09:41 +0100

    uprava flashmessages

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -5,8 +5,12 @@ declare(strict_types = 1);
 namespace App\Presenters;
 
 use App\Services\IConfigurator;
-use IPub\FlashMessages\TFlashMessages;
-use Kdyby\Translation\Phrase;
+use IPub\FlashMessages\Components\Control;
+use IPub\FlashMessages\Components\IControl;
+use IPub\FlashMessages\Entities\IMessage;
+use IPub\FlashMessages\Entities\Message;
+use IPub\FlashMessages\FlashNotifier;
+use IPub\FlashMessages\Storage\IStorage;
 use Kdyby\Translation\Translator;
 use NAttreid\Cms\Configurator\Configurator;
 use NAttreid\Form\Factories\FormFactory;
@@ -30,20 +34,18 @@ abstract class BasePresenter extends Presenter
 
 	use TPresenter,
 		TemplateTrait,
-		TFlashMessages,
 		SecuredLinksPresenterTrait;
 
 	protected function startup()
 	{
 		parent::startup();
+		$this->initLocale();
+	}
 
-		// lokalizace
-		if (empty($this->locale)) {
-			$this->locale = $this->translator->getDefaultLocale();
-		}
-		Number::setLocale($this->locale);
-		Date::setLocale($this->locale);
-		$this->template->locale = $this->locale;
+	protected function beforeRender()
+	{
+		parent::beforeRender();
+		$this->redrawFlashMessages();
 	}
 
 	/* ###################################################################### */
@@ -85,6 +87,81 @@ abstract class BasePresenter extends Presenter
 	protected function translate(string $message, int $count = null, array $parameters = [], string $domain = null, string $locale = null): string
 	{
 		return $this->translator->translate($message, $count, $parameters, $domain, $locale);
+	}
+
+	private function initLocale()
+	{
+		if (empty($this->locale)) {
+			$this->locale = $this->translator->getDefaultLocale();
+		}
+
+		Number::setLocale($this->locale);
+		Date::setLocale($this->locale);
+		$this->template->locale = $this->locale;
+	}
+
+	/* ###################################################################### */
+	/*                             FlashMessages                              */
+
+	/** @var IControl */
+	private $flashMessagesFactory;
+
+	/** @var FlashNotifier */
+	protected $flashNotifier;
+
+	/** @var IStorage */
+	private $flashStorage;
+
+	/**
+	 * @param IControl $flashMessagesFactory
+	 * @param FlashNotifier $flashNotifier
+	 */
+	public function injectFlashMessages(IControl $flashMessagesFactory, FlashNotifier $flashNotifier, IStorage $flashStorage)
+	{
+		$this->flashMessagesFactory = $flashMessagesFactory;
+		$this->flashNotifier = $flashNotifier;
+		$this->flashStorage = $flashStorage;
+	}
+
+	/**
+	 * Store flash message
+	 *
+	 * @param string $message
+	 * @param string $level
+	 * @param string|null $title
+	 * @param bool $overlay
+	 * @param int|null $count
+	 * @param array|null $parameters
+	 *
+	 * @return IMessage
+	 */
+	public function flashMessage($message, $level = 'info', $title = null, $overlay = false, $count = null, $parameters = []): IMessage
+	{
+		return $this->flashNotifier->message($message, $level, $title, $overlay, $count, $parameters);
+	}
+
+	/**
+	 * Flash messages component
+	 *
+	 * @return Control
+	 */
+	protected function createComponentFlashMessages(): Control
+	{
+		return $this->flashMessagesFactory->create();
+	}
+
+	private function redrawFlashMessages()
+	{
+		if ($this->isAjax()) {
+			/* @var $messages Message[] */
+			$messages = $this->flashStorage->get(IStorage::KEY_MESSAGES, []);
+			foreach ($messages as $message) {
+				if (!$message->isDisplayed()) {
+					$this['flashMessages']->redrawControl();
+					break;
+				}
+			}
+		}
 	}
 
 	/* ###################################################################### */
