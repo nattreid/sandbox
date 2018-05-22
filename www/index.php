3d31cbe043c89e6bf58b0264373b37b473106d64
attreid <attreid@gmail.com>
2018-05-22 08:53:33 +0200

    https

--- www/index.php
+++ www/index.php
@@ -9,8 +9,9 @@ if (file_exists(__DIR__ . '/../temp/maintenance')) {
 	require '.maintenance.php';
 }
 
-if (!empty($_SERVER['HTTPS']) && 'off' !== $_SERVER['HTTPS']) {
-	$_SERVER['SERVER_PORT'] = 443;
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+	$_SERVER['HTTPS'] = 'on';
+	$_SERVER['SERVER_PORT'] = '443';
 }
 
 $container = require __DIR__ . '/../app/bootstrap.php';
